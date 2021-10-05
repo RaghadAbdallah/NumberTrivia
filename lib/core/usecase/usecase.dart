6e561978f4193c934151e557de1687ff81c88e16
RaghadAbdallah <91069369+RaghadAbdallah@users.noreply.github.com>
2021-10-05 15:14:19 +0300

    Delete lib directory

--- lib/core/usecase/usecase.dart
+++
@@ -1,8 +0,0 @@
-import 'package:dartz/dartz.dart';
-import 'package:project1/core/error/failures.dart';
-import 'package:project1/features/number_trivia/domain/entities/number_trivia.dart';
-
-abstract class UseCase <Type,Params>{
-  @override
-  Future<Either<Failure,Type>> call(Params params);
-}
--- lib/features/number_trivia/domain/repositories/number_trivia_repository.dart
+++
@@ -1,8 +0,0 @@
-import 'package:dartz/dartz.dart';
-import 'package:project1/core/error/failures.dart';
-import 'package:project1/features/number_trivia/domain/entities/number_trivia.dart';
-
-abstract class NumberTriviaRepository {
-  Future<Either<Failure,NumberTrivia>> getConcreteNumberTrivia(int number);
-  Future<Either<Failure,NumberTrivia>> getRandomNumberTrivia(int number);
-}
